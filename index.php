d3bbe32d9328e96a2fdc537f6862b0aa3edac415
Soumya <go4soumya@gmail.com>
2019-11-07 01:43:51 -0600

    added db con to index page

--- index.php
+++ index.php
@@ -11,6 +11,27 @@
   </h1>
    <?php
      echo "Hello World.. this is a test php application"
+     $dbhost = getenv("MYSQL_SERVICE_HOST");
+     $dbport = getenv("MYSQL_SERVICE_PORT");
+     $dbuser = getenv("databaseuser");
+     $dbpwd = getenv("databasepassword");
+     $dbname = getenv("databasename");
+     $connection = new mysqli($dbhost, $dbuser, $dbpwd, $dbname);
+     if ($connection->connect_errno) {
+      ?>
+        <p>Could not connect to the database </p>
+      <?php
+         exit();
+     } else {
+       ?>
+       <br><br>
+       <hr/>
+       <br>
+       <p>The app is connected to the database on the container running on <?php echo $dbhost ?>:<?php echo $dbport ?> of the Openshift container</p>
+       <p>Database name: <?php echo $dbname ?> </p>
+      <?php
+     }
+     $connection->close();
     ?>
   </center>
  </body>
